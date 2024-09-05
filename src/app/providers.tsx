a3a14babedc75c037a963508cdae2e3fca2e7f51
ammahmoudi <am.mahmoudi@outlook.com>
2024-09-05 19:53:44 +0330

    add base of toast

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,7 @@ import { UserProvider } from "@/context/UserContext";
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { Router } from "next/router";
+import ToastProvider from "./ToastProvider";
 
 type Props = {
 	children: React.ReactNode;
@@ -12,10 +13,12 @@ type Props = {
 export default function Providers({ children }: Props) {
 	const router = useRouter();
 	return (
+		<ToastProvider>
 		<UserProvider>
 			<NextUIProvider navigate={router.push}>
 				{children}
 			</NextUIProvider>
 		</UserProvider>
+		</ToastProvider>
 	);
 }
