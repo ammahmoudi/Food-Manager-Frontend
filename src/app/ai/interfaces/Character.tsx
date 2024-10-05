a00c292647d0dcc6206d72edfa4e3c46772082c2
soheilhomayoonfard <soheilhomayoonfard@gmail.com>
2024-10-05 16:07:50 +0330

    fully functional generate picture

--- src/app/ai/interfaces/Character.tsx
+++ src/app/ai/interfaces/Character.tsx
@@ -3,7 +3,7 @@ import Dataset from "./Dataset";
 export default interface Character {
     id: number;
     name: string;
-    image: string; // Added avatar field for characters
+    image: string; 
     loras: Record<string, string>; // Loras is an object with name-path pairs
     datasets: Dataset[];
   }
--- src/app/ai/interfaces/Dataset.tsx
+++ src/app/ai/interfaces/Dataset.tsx
@@ -1,5 +1,11 @@
+
 export default interface Dataset {
     id: number;
     name: string;
+    created_by: number;
+    crated_at: string;
+    character : number | null;
+    jobs? : number[];
+    images? : number[]
+    dataset_type: string
   }
-  
